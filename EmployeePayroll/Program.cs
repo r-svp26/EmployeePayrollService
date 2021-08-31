ef012d41f503d27df55195863d9b4c0858e57476
r-svp26 <riteshk6341@gmail.com>
2021-08-31 16:46:26 +0530

    [Ritesh] Add: Retrieve the data from database

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Data;
 
 namespace EmployeePayroll
 {
+    /// <summary>
+    /// main class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Payroll Service");
+            EmployeeOperation employeeOperation = new EmployeeOperation();
+            employeeOperation.GetAllEmployeeDetail();
         }
     }
 }
